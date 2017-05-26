cea5cad90052f76a925c2ef217ecf790c8065798
Jamison Rubino <jamison.rubino@gmail.com>
2017-05-26 21:21:52 +0000

    Checkpoint 12 complete.

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
-
-  get 'welcome/about'
 
   get 'welcome/index'
 
   get 'welcome/about'
+  
+  get 'welcome/faq'
+  
   
   root 'welcome#index'
 
